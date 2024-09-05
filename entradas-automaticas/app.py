ae2e77fcb81545b409e55e244cb9cf13cc563f92
João Rhoden <j.machado02@edu.pucrs.br>
2024-09-05 14:04:24 -0300

    att

--- entradas-automaticas/app.py
+++ entradas-automaticas/app.py
@@ -18,7 +18,7 @@ bot = Bot(token=TOKEN)
 service = Service("/usr/local/bin/chromedriver")
 
 chrome_options = Options()
-#chrome_options.add_argument("--headless")  
+chrome_options.add_argument("--headless")  
 chrome_options.add_argument("--disable-gpu")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
